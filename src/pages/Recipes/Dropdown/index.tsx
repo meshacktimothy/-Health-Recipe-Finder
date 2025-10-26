0be397a1eb26375f9f025adcfa76f2ade8f49179
christianahade <olasubomistephen8@gmail.com>
2025-10-26 12:10:31 +0100

    index.tsx

--- src/pages/Recipes/Dropdown/index.tsx
+++ src/pages/Recipes/Dropdown/index.tsx
@@ -20,76 +20,71 @@ import { useState } from "react";
 export default function Dropdown() {
   const [openProfile, setopenProfile] = useState(false);
   const [openMaxCook, setopenMaxCook] = useState(false);
+  const [selectedPrepTime, setSelectedPrepTime] = useState<string | null>(null);
+  const [selectedCookTime, setSelectedCookTime] = useState<string | null>(null);
+
+  const handleClearPrep = () => {
+    setSelectedPrepTime(null);
+    setopenProfile(false);
+  };
+
+  const handleClearCook = () => {
+    setSelectedCookTime(null);
+    setopenMaxCook(false);
+  };
 
   return (
     <DropdownContainer>
       <MaxPrepAndMaxCook>
         <MaxPrepContainer>
           <MaxTimeContainer onClick={() => setopenProfile((prev) => !prev)}>
-            <Text>Max Prep Time </Text>
+            <Text>{selectedPrepTime ? `${selectedPrepTime} minutes` : "Max Prep Time"}</Text>
             <DropImgs open={openProfile}>
               <DropImage />
             </DropImgs>
           </MaxTimeContainer>
+
           {openProfile && (
             <MainList>
-              <ListItem>
-                <CheckRadio type="radio" name="Cheackbox"></CheckRadio> 0
-                minutes
+              <ListItem onClick={() => setSelectedPrepTime("0")}>
+                <CheckRadio type="radio" checked={selectedPrepTime === "0"} readOnly /> 0 minutes
               </ListItem>
-              <ListItem>
-                <CheckRadio type="radio" name="Cheackbox"></CheckRadio> 5
-                minutes
+              <ListItem onClick={() => setSelectedPrepTime("5")}>
+                <CheckRadio type="radio" checked={selectedPrepTime === "5"} readOnly /> 5 minutes
               </ListItem>
-              <ListItem>
-                <CheckRadio type="radio" name="Cheackbox"></CheckRadio> 10
-                minutes
+              <ListItem onClick={() => setSelectedPrepTime("10")}>
+                <CheckRadio type="radio" checked={selectedPrepTime === "10"} readOnly /> 10 minutes
               </ListItem>
-
-              <ListItem>Clear</ListItem>
+              <ListItem onClick={handleClearPrep} style={{ cursor: "pointer" }}>
+                Clear
+              </ListItem>
             </MainList>
           )}
         </MaxPrepContainer>
 
         <MaxPrepContainer>
           <MaxTimeContainer onClick={() => setopenMaxCook((prev) => !prev)}>
-            <Text>Max Prep Time </Text>
+            <Text>{selectedCookTime ? `${selectedCookTime} minutes` : "Max Cook Time"}</Text>
             <DropImgss open={openMaxCook}>
               <DropImage />
             </DropImgss>
           </MaxTimeContainer>
+
           {openMaxCook && (
             <MainList>
-              <ListItem>
-                <CheckRadio type="radio" name="Cheackbox"></CheckRadio> 0
-                minutes
+              {["0", "5", "10", "15", "20"].map((time) => (
+                <ListItem key={time} onClick={() => setSelectedCookTime(time)}>
+                  <CheckRadio type="radio" checked={selectedCookTime === time} readOnly /> {time} minutes
+                </ListItem>
+              ))}
+              <ListItem onClick={handleClearCook} style={{ cursor: "pointer" }}>
+                Clear
               </ListItem>
-              <ListItem>
-                <CheckRadio type="radio" name="Cheackbox"></CheckRadio> 5
-                minutes
-              </ListItem>
-              <ListItem>
-                <CheckRadio type="radio" name="Cheackbox"></CheckRadio> 10
-                minutes
-              </ListItem>
-              <ListItem>
-                <CheckRadio type="radio" name="Cheackbox"></CheckRadio> 10
-                minutes
-              </ListItem>
-              <ListItem>
-                <CheckRadio type="radio" name="Cheackbox"></CheckRadio> 15
-                minutes
-              </ListItem>
-              <ListItem>
-                <CheckRadio type="radio" name="Cheackbox"></CheckRadio> 20
-                minutes
-              </ListItem>
-
-              <ListItem>Clear</ListItem>
             </MainList>
           )}
         </MaxPrepContainer>
       </MaxPrepAndMaxCook>
+
       <SearchContainer>
         <Searchimage>
           <SearchImg />
